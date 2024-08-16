75441f395206f961e4a7104352c0e3e4cff87ad9
ymougenel <ymougenel@takima.fr>
2024-08-17 00:42:55 +0200

    refactor: clean code and re-ident

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Project from './cards/Project';
 import ProjectList from "./cards/ProjectList";
 
 function App() {
-  return (
-    <div className="App">
-      <ProjectList />
-      {/*<header className="App-header">*/}
-      {/*  <img src={logo} className="App-logo" alt="logo" />*/}
-      {/*  <p>*/}
-      {/*    Edit <code>src/App.tsx</code> and save to reload.*/}
-      {/*  </p>*/}
-      {/*  <a*/}
-      {/*    className="App-link"*/}
-      {/*    href="https://reactjs.org"*/}
-      {/*    target="_blank"*/}
-      {/*    rel="noopener noreferrer"*/}
-      {/*  >*/}
-      {/*    Learn React*/}
-      {/*  </a>*/}
-      {/*</header>*/}
-    </div>
-  );
+    return (
+        <div className="App">
+            <ProjectList/>
+            {/*<header className="App-header">*/}
+            {/*  <img src={logo} className="App-logo" alt="logo" />*/}
+            {/*  <p>*/}
+            {/*    Edit <code>src/App.tsx</code> and save to reload.*/}
+            {/*  </p>*/}
+            {/*  <a*/}
+            {/*    className="App-link"*/}
+            {/*    href="https://reactjs.org"*/}
+            {/*    target="_blank"*/}
+            {/*    rel="noopener noreferrer"*/}
+            {/*  >*/}
+            {/*    Learn React*/}
+            {/*  </a>*/}
+            {/*</header>*/}
+        </div>
+    );
 }
 
 export default App;
