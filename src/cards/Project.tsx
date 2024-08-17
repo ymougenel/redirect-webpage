3c893db918faac463c3f53bede3baec98679e1ae
ymougenel <ymougenel@takima.fr>
2024-08-17 23:37:07 +0200

    feat: add github link

--- src/cards/Project.tsx
+++ src/cards/Project.tsx
@@ -6,13 +6,16 @@ import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import {CardActions, CardMedia, Typography} from "@mui/material";
 
-function Project({projectTitle, imageUrl, projectDetails, moreDetails, siteUrl, learnMoreLink}: { //TODO define righ props types
+function Project({projectTitle, imageUrl, projectDetails, moreDetails, status, siteUrl, githubURL, learnMoreLink}: { //TODO define righ props types
     projectTitle: any,
     imageUrl: any,
     projectDetails: any,
     moreDetails: any,
+    status: any,
     siteUrl: any
-    learnMoreLink: any
+    learnMoreLink: any,
+    githubURL: any,
+
 }) {
     // TODO: use global borderTop config (var???)
     return (
@@ -30,11 +33,21 @@ function Project({projectTitle, imageUrl, projectDetails, moreDetails, siteUrl, 
                     <Typography variant="body2" color="text.secondary">
                         {projectDetails}
                     </Typography>
+                    {/*<p>{status}</p>*/}
                 </CardContent>
-                <CardActions style={{borderTop: ' 2px solid rgba(160,160,160,0.2)', marginLeft: "5%" }}>
+                <CardActions style={{borderTop: ' 2px solid rgba(160,160,160,0.2)', marginLeft: "5%"}}>
                     <Button size="small" href={siteUrl}>See Website</Button>
+                    {/*IF MORE DETAILS TO DEPLAY*/}
                     {moreDetails ? (
                         <Button size="small" href={learnMoreLink}>{moreDetails}</Button>
+                    ) : (<p></p>)}
+                    {githubURL !== "NONE" ? (
+                        // <div style={{display: "flex", flexDirection: "row", alignItems: "flex-end"}}>
+                            <a href={githubURL}>
+                                <img style={{height: "30px"}}
+                                     src="https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png"/>
+                            </a>
+                        // </div>
                     ) : (<p></p>)}
                 </CardActions>
             </Card>
--- src/cards/ProjectList.tsx
+++ src/cards/ProjectList.tsx
@@ -11,31 +11,37 @@ function ProjectList() {
             projectDetails: "A blog covering all my past and current projects",
             moreDetails: "",
             status: ["Running", "MAINTAINED"],
-            siteURL: "https://blog.ymougenel.com/"
+            siteURL: "https://blog.ymougenel.com/",
+            githubURL: "https://github.com/ymougenel/ymougenel.github.io",
         },
         {
             imageUrl: "./projects/histoire_de_rues.png",
             projectTitle: "Histoire de rues",
             projectDetails: "Un site web  présentant une vision cartographique de l'histoire de Paris, en collaboration avec le youtubeur/historien Sur le champ",
             moreDetails: "",
-            siteURL: "https://streets-highlight.netlify.app/",
             status: ["Running, MAINTAINED"],
+            siteURL: "https://streets-highlight.netlify.app/",
+            githubURL: "https://github.com/ymougenel/streets_highlight"
         },
         {
             imageUrl: "https://www.naowak.fr/articles/kavu-poc/preview.png",
             projectTitle: "Kavu Déchet",
-            projectDetails: "An environnemental  project, see article https://www.naowak.fr/article/kavu-poc",
+            projectDetails: "An environnemental project in Mayotte",
             moreDetails: "See article",
             learnMoreLink: "https://www.naowak.fr/article/kavu-poc",
             siteURL: "https://streets-highlight.netlify.app/",
-            status: "[Unkown]"
+            status: "[Unkown]",
+            githubURL: "https://github.com/Kavu-Dechet",
+
         },
         {
             imageUrl: "https://materializecss.com/images/sample-1.jpg",
             projectTitle: "Enseignement SNT/NSI",
             projectDetails: "A simple web site containing most of my teacher courses. Covers high school computer lessons",
             siteURL: "http://nsi.ymougenel.com/",
-            status: "[Running, NOT_MAINTAINED]"
+            status: "[Running, NOT_MAINTAINED]",
+            githubURL: "NONE"
+
         },
         // {
         //     imageUrl: "https://mui.com/static/images/cards/contemplative-reptile.jpg",
@@ -50,7 +56,7 @@ function ProjectList() {
             {projects.map((item) => (
                 <Project projectTitle={item.projectTitle} imageUrl={item.imageUrl}
                          projectDetails={item.projectDetails} moreDetails={item.moreDetails} siteUrl={item.siteURL}
-                         learnMoreLink={item.learnMoreLink}/>
+                         learnMoreLink={item.learnMoreLink} status={item.status} githubURL={item.githubURL}/>
 
             ))}
 
