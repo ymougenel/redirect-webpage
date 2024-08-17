cd0a62b219abd1423a26e92a5e3647f042ade10f
ymougenel <ymougenel@takima.fr>
2024-08-17 22:21:17 +0200

    feat: complete first projects

--- src/cards/Project.tsx
+++ src/cards/Project.tsx
@@ -6,20 +6,25 @@ import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import {CardActions, CardMedia, Typography} from "@mui/material";
 
-function Project({projectTitle, imageUrl, projectDetails}: { projectTitle: any, imageUrl: any, projectDetails: any }) {
+function Project({projectTitle, imageUrl, projectDetails, moreDetails, siteUrl, learnMoreLink}: { //TODO define righ props types
+    projectTitle: any,
+    imageUrl: any,
+    projectDetails: any,
+    moreDetails: any,
+    siteUrl: any
+    learnMoreLink: any
+}) {
     let index = 0;
-    // let imageUrl= "https://materializecss.com/images/sample-1.jpg";
-    // let projectTitle = "Enseignement SNT/NSI";
-    // let projectDetails = "A simple web site containing most of my teacher courses. Covers high school computer lessons";
+   // TODO: use global borderTop config (var???)
     return (
         <div>
-            <Card sx={{maxWidth: 345}}>
+            <Card sx={{ width: 420, boxShadow: "11px 7px 5px 2px black"}}>
                 <CardMedia
                     sx={{height: 140}}
                     image={imageUrl}
                     title="green iguana"
                 />
-                <CardContent>
+                <CardContent style={{ borderTop: ' 2px solid rgba(160,160,160,0.2)'}} >
                     <Typography gutterBottom variant="h5" component="div">
                         {projectTitle}
                     </Typography>
@@ -27,9 +32,11 @@ function Project({projectTitle, imageUrl, projectDetails}: { projectTitle: any, 
                         {projectDetails}
                     </Typography>
                 </CardContent>
-                <CardActions>
-                    <Button size="small">Share</Button>
-                    <Button size="small">Learn More</Button>
+                <CardActions style={{ borderTop: ' 2px solid rgba(160,160,160,0.2)'}} >
+                    <Button size="small" href={siteUrl}>See Website</Button>
+                    {moreDetails ? (
+                        <Button size="small" href={learnMoreLink}>{moreDetails}</Button>
+                    ) : (<p></p>)}
                 </CardActions>
             </Card>
         </div>
