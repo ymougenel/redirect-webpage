9ef80e738709089d9ab4ab29680b47d4a69dfdd9
ymougenel <yann.mougenel@gmail.com>
2026-03-09 18:41:34 +0100

    style: implement basic project style

--- redirect-app/src/app/app-module.ts
+++ redirect-app/src/app/app-module.ts
@@ -1,15 +1,21 @@
 import { NgModule, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { Header } from './header/header';
+import { ProjectCard } from './project-card/project-card';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    Header,
+    ProjectCard
   ],
   imports: [
     BrowserModule,
+    CommonModule,
     AppRoutingModule
   ],
   providers: [
--- redirect-app/src/app/app.ts
+++ redirect-app/src/app/app.ts
@@ -1,4 +1,5 @@
 import { Component, signal } from '@angular/core';
+import { Project } from './project.model';
 
 @Component({
   selector: 'my-root',
@@ -8,4 +9,31 @@ import { Component, signal } from '@angular/core';
 })
 export class App {
   protected readonly title = signal('redirect-app');
+
+  projects: Project[] = [
+    {
+      id: 1,
+      title: 'AWS Site Screen',
+      description: 'A project related to AWS site screening.',
+      image: 'assets/projects/aws_site_screen.png',
+      projectLink: '#',
+      githubLink: '#'
+    },
+    {
+      id: 2,
+      title: 'COVID-19 Tracker',
+      description: 'An application to track COVID-19 data.',
+      image: 'assets/projects/covid19.png',
+      projectLink: '#',
+      githubLink: '#'
+    },
+    {
+      id: 3,
+      title: 'Histoire de Rues',
+      description: 'A project about street histories.',
+      image: 'assets/projects/histoire_de_rues.png',
+      projectLink: '#',
+      githubLink: '#'
+    }
+  ];
 }
